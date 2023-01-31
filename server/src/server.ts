9ca3deeb253e2db103cb7ac736accfd9abc50793
FelipeAlves <profelipealves@outlook.com>
2023-01-31 19:08:16 -0300

    feat: add cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response, NextFunction } from "express";
 import { router } from "./routes";
 import "express-async-errors";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use(router);
 
