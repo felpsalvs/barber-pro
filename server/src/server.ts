3b98e6b24bf1f5d634b700dfac39db5c847f20da
FelipeAlves <profelipealves@outlook.com>
2023-01-31 19:06:43 -0300

    feat: add express-async-errors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
-import express, { Request, Response, NextFunction } from 'express';
-import { router } from './routes';
+import express, { Request, Response, NextFunction } from "express";
+import { router } from "./routes";
+import "express-async-errors";
 
 const app = express();
 
@@ -7,4 +8,17 @@ app.use(express.json());
 
 app.use(router);
 
-app.listen(3333, () => console.log('Server is running! :D'))
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      error: err.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal Server Error",
+  });
+});
+
+app.listen(3333, () => console.log("Server is running! :D"));
