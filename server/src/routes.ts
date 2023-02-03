01c9ee8f7a44e0b7d1de1d18cae633018d5a8247
FelipeAlves <profelipealves@outlook.com>
2023-02-03 06:41:14 -0300

    ✨ feat: add auth in login

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,12 +1,12 @@
 import { Router, Request, Response } from 'express';
 
 import { CreateUserController } from './controllers/user/CreateUserController';
+import { AuthUserController } from './controllers/user/AuthUserController';
 
 const router = Router();
 
-//router.get('/teste', (req: Request, res: Response) => {
-  //eturn res.json({ message: 'Hello World' });
-//});
 // ROUTES USERS
 router.post('/users', new CreateUserController().handle)
+router.post('/session', new AuthUserController().handle)
+
 export { router };
