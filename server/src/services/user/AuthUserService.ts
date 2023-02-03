cc79be52eead5042bbce8f0b050e8cb474c41f14
FelipeAlves <profelipealves@outlook.com>
2023-02-03 18:48:29 -0300

    🚑 add jwt

--- server/src/services/user/AuthUserService.ts
+++ server/src/services/user/AuthUserService.ts
@@ -1,5 +1,6 @@
 import prismaClient from "../../prisma";
 import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 
 interface AuthUserRequest {
   email: string;
@@ -20,14 +21,38 @@ class AuthUserService {
     if (!user) {
       throw new Error("Email/Password incorrect");
     }
-    
+
     const passwordMatch = await compare(password, user?.password);
 
     if (!passwordMatch) {
       throw new Error("Email/Password incorrect");
     }
 
-    return {opa: 'opa'}
+    const token = sign(
+      {
+        email: user.email,
+        name: user.name,
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: "30d",
+      }
+    );
+
+    return {
+      id: user?.id,
+      name: user?.name,
+      email: user?.email,
+      endereco: user?.endereco,
+      token: token,
+      subscriptions: user.subscriptions
+        ? {
+            id: user?.subscriptions?.id,
+            status: user?.subscriptions?.status,
+          }
+        : null,
+    };
   }
 }
 
