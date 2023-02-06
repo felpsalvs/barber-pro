290155bd22a38fafce5a971f0872eb0180838cf4
FelipeAlves <profelipealves@outlook.com>
2023-02-06 07:49:34 -0300

    ✨ feat: add details of users

--- server/src/services/user/DetailUserService.ts
+++ server/src/services/user/DetailUserService.ts
@@ -2,7 +2,26 @@ import prismaClient from "../../prisma";
 
 class UserDetailService {
   async execute(user_id: string) {
-    return { user_id: user_id };
+    const user = await prismaClient.user.findFirst({
+      where: {
+        id: user_id,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        endereco: true,
+        subscriptions: {
+          select: {
+            id: true,
+            priceId: true,
+            status: true,
+          },
+        }
+      },
+    });
+
+    return user;
   }
 }
 
