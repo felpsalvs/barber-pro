2347bc00de3a7be8d3821d4c92413bfec1caa518
FelipeAlves <profelipealves@outlook.com>
2023-02-06 09:13:46 -0300

    ✨ feat: add service of controllers

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -4,6 +4,7 @@ import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
 import { isAuthenticated } from './middlewares/isAuthenticated';
+import { UpdateUserController } from './controllers/user/UpdateUserController';
 
 const router = Router();
 
@@ -11,5 +12,6 @@ const router = Router();
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
 router.get('/me', isAuthenticated, new DetailUserController().handle)
+router.put('/users', isAuthenticated, new UpdateUserController().handle)
 
 export { router };
