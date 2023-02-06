bc72c329285eb3c47ccd8cdba936c300af0811b6
FelipeAlves <profelipealves@outlook.com>
2023-02-06 13:01:57 -0300

    ✨ feat: list cut templates

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -7,6 +7,7 @@ import { isAuthenticated } from './middlewares/isAuthenticated';
 import { UpdateUserController } from './controllers/user/UpdateUserController';
 
 import { CreateHaircutController } from './controllers/haircut/CreateHaircutController';
+import { ListHaircutController } from './controllers/haircut/ListHaircutController';
 
 const router = Router();
 
@@ -18,5 +19,6 @@ router.put('/users', isAuthenticated, new UpdateUserController().handle)
 
 // ROUTES HAIRCUTS
 router.post('/haircut', isAuthenticated, new CreateHaircutController().handle)
+router.get('/haircuts', isAuthenticated, new ListHaircutController().handle)
 
 export { router };
