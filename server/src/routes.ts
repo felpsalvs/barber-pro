9d500d4854fc58ab101a535e6d05db67a23eae14
FelipeAlves <profelipealves@outlook.com>
2023-02-06 19:27:35 -0300

    ✨ feat: finished service

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -15,6 +15,7 @@ import { DetailHaircutController } from './controllers/haircut/DetailHaircutCont
 
 import { NewScheduleController } from './controllers/schedule/NewScheduleController';
 import { ListScheduleController } from './controllers/schedule/ListScheduleController';
+import { FinishScheduleController } from './controllers/schedule/FinishScheduleController';
 
 const router = Router();
 
@@ -35,5 +36,6 @@ router.get('/haircut/detail', isAuthenticated, new DetailHaircutController().han
 // ROUTES SCHEDULES
 router.post('/schedule', isAuthenticated, new NewScheduleController().handle)
 router.get('/schedule', isAuthenticated, new ListScheduleController().handle)
+router.delete('/schedule', isAuthenticated, new FinishScheduleController().handle)
 
 export { router };
---
+++ server/src/services/schedule/FinishScheduleService.ts
@@ -0,0 +1,39 @@
+import prismaClient from "../../prisma";
+
+interface FinishRequest {
+  schedule_id: string;
+  user_id: string;
+}
+
+class FinishScheduleService {
+  async execute({ schedule_id, user_id }: FinishRequest) {
+    if (schedule_id === "" || user_id === "") {
+      throw new Error("Error");
+    }
+
+    try {
+      const belongsToUser = await prismaClient.service.findFirst({
+        where: {
+          id: schedule_id,
+          user_id: user_id,
+        },
+      });
+
+      if (!belongsToUser) {
+        throw new Error("Not authorized");
+      }
+      await prismaClient.service.delete({
+        where: {
+          id: schedule_id,
+        },
+      });
+
+      return { message: "Completed successfully" };
+    } catch (err) {
+      console.log(err);
+      throw new Error("Error");
+    }
+  }
+}
+
+export { FinishScheduleService };
