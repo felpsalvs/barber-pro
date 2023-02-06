56164d3b88e4ec570b28393fc81f14d021daef6d
FelipeAlves <profelipealves@outlook.com>
2023-02-06 13:56:29 -0300

    ✨ feat: check user signature

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -9,6 +9,7 @@ import { UpdateUserController } from './controllers/user/UpdateUserController';
 import { CreateHaircutController } from './controllers/haircut/CreateHaircutController';
 import { ListHaircutController } from './controllers/haircut/ListHaircutController';
 import { UpdateHaircutController } from './controllers/haircut/UpdateHaircutController';
+import { CheckSubscriptionController } from './controllers/haircut/CheckSubscriptionController';
 
 const router = Router();
 
@@ -22,5 +23,6 @@ router.put('/users', isAuthenticated, new UpdateUserController().handle)
 router.post('/haircut', isAuthenticated, new CreateHaircutController().handle)
 router.get('/haircuts', isAuthenticated, new ListHaircutController().handle)
 router.put('/haircut', isAuthenticated, new UpdateHaircutController().handle)
+router.get('/haircut/check', isAuthenticated, new CheckSubscriptionController().handle)
 
 export { router };
