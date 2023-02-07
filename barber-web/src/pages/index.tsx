40cecc53bfffce8531c3f38277a428369991ea84
FelipeAlves <profelipealves@outlook.com>
2023-02-07 06:43:55 -0300

    🚀 feat: initial commit

---
+++ barber-web/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home(){
+  return (
+    <>
+      <Head>
+        <title>BarberPRO - O sistema da barbeiro</title>
+      </Head>
+      <h1>Home</h1>
+    </>
+  )
+}
