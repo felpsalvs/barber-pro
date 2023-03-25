9547d22fa85fd4bd9a2ac700082c4d3cd8dfeaa0
Felipe Alves <profelipealves@outlook.com>
2023-03-25 09:41:32 -0300

    ✨ feat: listing queue customers

--- barber-web/src/pages/dashboard/index.tsx
+++ barber-web/src/pages/dashboard/index.tsx
@@ -11,8 +11,26 @@ import { canSSRAuth } from "../../utils/canSSRAuth";
 import { Sidebar } from "@/components/sidebar";
 import Link from "next/link";
 import { IoMdPerson } from "react-icons/io";
+import { setupAPIClient } from "@/services/api";
+import { useState } from "react";
 
-export default function Dashboard() {
+export interface ScheduleItem {
+  id: string;
+  customer: string;
+  haircut: {
+    id: string;
+    name: string;
+    price: string | number;
+    user_id: string;
+  };
+}
+
+interface DashboardProps {
+  schedule: ScheduleItem[];
+}
+
+export default function Dashboard({ schedule }: DashboardProps) {
+  const [list, setList] = useState(schedule);
   const [isMobile] = useMediaQuery("(max-width: 500px)");
 
   return (
@@ -31,43 +49,46 @@ export default function Dashboard() {
             </Link>
           </Flex>
 
-          <ChakraLink
-            w="100%"
-            m={0}
-            p={0}
-            mt={1}
-            bg="transparent"
-            style={{ textDecoration: "none" }}
-          >
-            <Flex
+          {list.map(item => (
+            <ChakraLink
+            key={item?.id}
               w="100%"
-              direction={isMobile ? "column" : "row"}
-              p={4}
-              rounded={4}
-              mb={4}
-              bg="barber.400"
-              justify="space-between"
-              align={isMobile ? "flex-start" : "center"}
+              m={0}
+              p={0}
+              mt={1}
+              bg="transparent"
+              style={{ textDecoration: "none" }}
             >
               <Flex
-                direction="row"
-                mb={isMobile ? 2 : 0}
-                align="center"
-                justify="center"
+                w="100%"
+                direction={isMobile ? "column" : "row"}
+                p={4}
+                rounded={4}
+                mb={2}
+                bg="barber.400"
+                justify="space-between"
+                align={isMobile ? "flex-start" : "center"}
               >
-                <IoMdPerson size={28} color="#f1f1f1" />
-                <Text fontWeight="bold" ml={4} noOfLines={2}>
-                  Felps
+                <Flex
+                  direction="row"
+                  mb={isMobile ? 2 : 0}
+                  align="center"
+                  justify="center"
+                >
+                  <IoMdPerson size={28} color="#f1f1f1" />
+                  <Text fontWeight="bold" ml={4} noOfLines={2}>
+                    {item?.customer}
+                  </Text>
+                </Flex>
+                <Text fontWeight="bold" mb={isMobile ? 2 : 0}>
+                  {item?.haircut?.name}
+                </Text>
+                <Text fontWeight="bold" mb={isMobile ? 2 : 0}>
+                  R$ {item?.haircut?.price}
                 </Text>
               </Flex>
-              <Text fontWeight="bold" mb={isMobile ? 2 : 0}>
-                Corte completo
-              </Text>
-              <Text fontWeight="bold" mb={isMobile ? 2 : 0}>
-                29.00
-              </Text>
-            </Flex>
-          </ChakraLink>
+            </ChakraLink>
+          ))}
         </Flex>
       </Sidebar>
     </>
@@ -75,7 +96,21 @@ export default function Dashboard() {
 }
 
 export const getServerSideProps = canSSRAuth(async (ctx) => {
-  return {
-    props: {},
-  };
+  try {
+    const apiClient = setupAPIClient(ctx);
+    const response = await apiClient.get("/schedule");
+
+    return {
+      props: {
+        schedule: response.data,
+      },
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      props: {
+        schedule: [],
+      },
+    };
+  }
 });
