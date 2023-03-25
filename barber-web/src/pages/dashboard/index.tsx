2e1d363b53ad8833f766ab38ac2d58a2bd9bd452
Felipe Alves <profelipealves@outlook.com>
2023-03-25 08:01:53 -0300

    ✨ feat: create page services

--- barber-web/src/pages/dashboard/index.tsx
+++ barber-web/src/pages/dashboard/index.tsx
@@ -1,17 +1,73 @@
 import Head from "next/head";
-import { Flex, Text } from "@chakra-ui/react";
+import {
+  Button,
+  Flex,
+  Heading,
+  Text,
+  Link as ChakraLink,
+  useMediaQuery,
+} from "@chakra-ui/react";
 import { canSSRAuth } from "../../utils/canSSRAuth";
 import { Sidebar } from "@/components/sidebar";
+import Link from "next/link";
+import { IoMdPerson } from "react-icons/io";
 
 export default function Dashboard() {
+  const [isMobile] = useMediaQuery("(max-width: 500px)");
+
   return (
     <>
       <Head>
         <title>BarberPRO - Minha barbearia</title>
       </Head>
       <Sidebar>
-        <Flex>
-          <Text>Bem-vindo ao Dashboard</Text>
+        <Flex direction="column" align="flex-start" justify="flex-start">
+          <Flex w="100%" direction="row" align="center" justify="flex-start">
+            <Heading fontSize="3xl" mt={4} mb={4} mr={4}>
+              Agenda
+            </Heading>
+            <Link href="/new">
+              <Button>Registrar</Button>
+            </Link>
+          </Flex>
+
+          <ChakraLink
+            w="100%"
+            m={0}
+            p={0}
+            mt={1}
+            bg="transparent"
+            style={{ textDecoration: "none" }}
+          >
+            <Flex
+              w="100%"
+              direction={isMobile ? "column" : "row"}
+              p={4}
+              rounded={4}
+              mb={4}
+              bg="barber.400"
+              justify="space-between"
+              align={isMobile ? "flex-start" : "center"}
+            >
+              <Flex
+                direction="row"
+                mb={isMobile ? 2 : 0}
+                align="center"
+                justify="center"
+              >
+                <IoMdPerson size={28} color="#f1f1f1" />
+                <Text fontWeight="bold" ml={4} noOfLines={2}>
+                  Felps
+                </Text>
+              </Flex>
+              <Text fontWeight="bold" mb={isMobile ? 2 : 0}>
+                Corte completo
+              </Text>
+              <Text fontWeight="bold" mb={isMobile ? 2 : 0}>
+                29.00
+              </Text>
+            </Flex>
+          </ChakraLink>
         </Flex>
       </Sidebar>
     </>
